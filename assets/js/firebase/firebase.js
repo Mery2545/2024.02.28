669b83040297d107522a3ca45c998deb8f9e2877
Mery2545 <calderonleivam@gmail.com>
2024-02-28 20:10:24 -0500

    cambio 1

---
+++ assets/js/firebase/firebase.js
@@ -0,0 +1,43 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-app.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-auth.js"
+import {
+    getFirestore,
+    collection,
+    doc,
+    addDoc,
+    getDoc,
+    updateDoc,
+    deleteDoc,
+    onSnapshot
+} from "https://www.gstatic.com/firebasejs/10.8.0/firebase-firestore.js";
+
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyARKpf0b4Z4_Yu8r2XOngPmS51rtUPoC2k",
+    authDomain: "lingo-disenojs-9010b.firebaseapp.com",
+    projectId: "lingo-disenojs-9010b",
+    storageBucket: "lingo-disenojs-9010b.appspot.com",
+    messagingSenderId: "956436316561",
+    appId: "1:956436316561:web:cb07b12dbff9ab3b091a30"
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
+export const db = getFirestore();
+
+// Funciones del CRUD
+export const createTask = (title, description, userName, date, time) => addDoc(collection(db, "tasks"), {title, description, userName, date, time});
+
+export const getTask = id => getDoc(doc(db, "tasks", id));
+
+export const onGetTask = callback => onSnapshot(collection(db, "tasks"), callback);
+
+export const updateTask = (id, newFields) => updateDoc(doc(db, "tasks", id), newFields);
+
+export const deleteTask = id => deleteDoc(doc(db, "tasks", id));
